333d00decff9c72558023edbee5c6d0f62c55879
Avishek <abishekyangkhurung@gmail.com>
2025-06-15 08:31:42 +0545

    all files added

---
+++ hello.py
@@ -0,0 +1,10 @@
+# def greet(name):
+#     return f'Hello, {name}'
+
+import greet as gt
+
+# print(gt.greet('Ram'))
+# print(gt.greet('hari'))
+
+if __name__ == '__main__':
+    print(gt.greet('gopal'))
